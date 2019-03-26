15e23bf31a1bcf1669d3d6293596d99014d1f7b4
Jim Dersch <dncrjim@gmail.com>
2019-03-25 20:34:21 -0400

    Work @ Class

--- app/src/main/java/com/dncrjim/metronome/MainActivity.kt
+++ app/src/main/java/com/dncrjim/metronome/MainActivity.kt
@@ -15,8 +15,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var handler: Handler
     var tempoList = mutableListOf<Int>(80, 100, 120, 140)
 
-        //Todo: make tempoList persistent across app and saves.
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,15 +26,14 @@ class MainActivity : AppCompatActivity() {
         newTempoButton.setOnClickListener {
             inputNewTempo()
         }
+
         //Todo: Recycler View for recently used tempos
-
-
     }
 
-    private fun inputNewTempo() {
+    fun inputNewTempo() {
         val dialogBuilder = AlertDialog.Builder(this)
         val inflater = this.layoutInflater
-        val dialogView = inflater.inflate(R.layout.new_tempo_dialog, null)
+        val dialogView = inflater.inflate(R.layout.custom_dialog, null)
         dialogBuilder.setView(dialogView)
 
         val editText = dialogView.findViewById<View>(R.id.editTempoText) as EditText
@@ -44,20 +41,15 @@ class MainActivity : AppCompatActivity() {
         dialogBuilder.setTitle("Add New Tempo")
         dialogBuilder.setPositiveButton("OK") {dialog, whichButton ->
 
-            val tempoAsInt = editText.text.toString().toInt()
+            var tempoAsInt = editText.text.toString().toInt()
             when (tempoAsInt) {
                 !in 36..300 -> Toast . makeText (this, "Error: Tempo must be between 36 and 300", Toast.LENGTH_LONG).show()
-                in tempoList -> {
-                    val intent = Intent(this, TempoActivity::class.java)
-                    intent.putExtra("SEND_TEMPO",tempoAsInt)
-                    startActivity(intent)
-                }
                 else -> {
                     tempoList.add(tempoAsInt)
 
                     //move to other activity
                     val intent = Intent(this, TempoActivity::class.java)
-                    intent.putExtra("SEND_TEMPO",tempoAsInt)
+                    intent.putExtra("currentTempo", tempoAsInt)
                     startActivity(intent)
                 }
                 //also close dialog box?
--- app/src/main/java/com/dncrjim/metronome/TempoActivity.kt
+++ app/src/main/java/com/dncrjim/metronome/TempoActivity.kt
@@ -22,5 +22,3 @@ class TempoActivity : AppCompatActivity() {
     }
 
 }
-
-
